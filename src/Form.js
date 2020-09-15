2ff780246de37cd4e11a5ba6c04e0f4c5e1b06c0
Benjamin Barnard <barnardwben@yahoo.com>
2020-09-15 18:43:20 +0200

    Added inputs, useState to Form Component

--- src/Form.js
+++ src/Form.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Form.css";
 
 const Form = () => {
+  const [person, setPerson] = useState({ firstName: "", email: "", age: "" });
+  const [people, setPeople] = useState([]);
+
   return (
     <>
       <article>
@@ -11,6 +14,15 @@ const Form = () => {
             <label htmlFor="firstName">Name : </label>
             <input type="text" id="firstName" name="firstName" />
           </div>
+          <div>
+            <label htmlFor="email">Email : </label>
+            <input type="email" id="email" name="email" />
+          </div>
+          <div>
+            <label htmlFor="age">Age : </label>
+            <input type="text" id="age" name="age" />
+          </div>
+          <button type="submit">Submit Contact Info.</button>
         </form>
       </article>
     </>
