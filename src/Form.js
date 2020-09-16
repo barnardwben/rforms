5c846715b43727af53d3fb5fbbe4d7aa18b46582
Benjamin Barnard <barnardwben@yahoo.com>
2020-09-16 23:48:54 +0200

    Added code for input fields and displaying person object

--- src/Form.js
+++ src/Form.js
@@ -1,9 +1,25 @@
 import React, { useState } from "react";
+import uuid from "react-uuid";
 import "./Form.css";
 
 const Form = () => {
   const [person, setPerson] = useState({ firstName: "", email: "", age: "" });
   const [people, setPeople] = useState([]);
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setPerson({ ...person, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (person.firstName && person.email && person.age) {
+      const newPerson = { ...person, id: uuid() };
+      setPeople([...people, newPerson]);
+      setPerson({ firstName: "", email: "", age: "" });
+    }
+  };
 
   return (
     <>
@@ -12,18 +28,48 @@ const Form = () => {
         <form className="form-container">
           <div>
             <label htmlFor="firstName">Name : </label>
-            <input type="text" id="firstName" name="firstName" />
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              value={person.firstName}
+              onChange={handleChange}
+            />
           </div>
           <div>
             <label htmlFor="email">Email : </label>
-            <input type="email" id="email" name="email" />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={person.email}
+              onChange={handleChange}
+            />
           </div>
           <div>
             <label htmlFor="age">Age : </label>
-            <input type="text" id="age" name="age" />
+            <input
+              type="text"
+              id="age"
+              name="age"
+              value={person.age}
+              onChange={handleChange}
+            />
           </div>
-          <button type="submit">Submit Contact Info.</button>
+          <button type="submit" onClick={handleSubmit}>
+            Submit Contact Info.
+          </button>
         </form>
+        {people.map((person) => {
+          const { id, firstName, email, age } = person;
+          return (
+            <div className="person-list" key={id}>
+              <h4>{firstName}</h4>
+              <p>{age}</p>
+              <p>{email}</p>
+            </div>
+          );
+        })}
       </article>
     </>
   );
